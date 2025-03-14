586bac32692f6782334fcbb72dd580f90230e4e1
ayankhan21 <ayankhanxyz00@gmail.com>
2025-03-15 00:46:08 +0530

    Implement data input/output streams for TCP client and server communication

--- tcp-connection/TCPClient.java
+++ tcp-connection/TCPClient.java
@@ -1,4 +1,6 @@
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
@@ -8,10 +10,23 @@ public class TCPClient {
         try (Socket socket = new Socket()) {
             // the socket instance resource is created in the special try , which releases the resource after the try block is executed
             socket.connect(new InetSocketAddress("127.0.0.1", 5001),1000);
+            System.out.println(new InetSocketAddress("127.0.0.1", 5001) + "this is the endpoint that is made from ip & port");
             // the connect takes 2 params , 1: endpoint , 2: timeout in miliseconds
             // Since we dont have an address we are creating one using InetSocketAddress
             // InetSocketAddress - Creates a socket address from a hostname and a port number.
             System.out.println("Connection Successful!");
+
+
+            DataInputStream dataIn = new DataInputStream(socket.getInputStream());
+            DataOutputStream dataOut = new DataOutputStream(socket.getOutputStream());
+
+            dataOut.writeUTF("Hello from the client!"); // writeUTF writes a string to the output stream
+
+            String serverMessage = dataIn.readUTF(); // readUTF reads a string from the input stream
+            System.out.println("Server: " + serverMessage);
+
+            dataIn.close();
+            dataOut.close();    
         }
     }
 }
--- tcp-connection/TCPServer.java
+++ tcp-connection/TCPServer.java
@@ -1,3 +1,5 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -18,6 +20,23 @@ public class TCPServer{
             String clientSocketIP = clientSocket.getInetAddress().toString(); // getting the IP address of the client
             int clientSocketPort = clientSocket.getPort(); // getting the port number of the client
             System.out.println("[IP: " + clientSocketIP + " ,Port: " + clientSocketPort +"]  " + "Client Connection Successful!");
+
+            DataInputStream dataIn = new DataInputStream(clientSocket.getInputStream());
+            DataOutputStream dataOut = new DataOutputStream(clientSocket.getOutputStream());
+
+            // We are creating a data input and output stream to read and write data to the client.
+
+            String clientMessage = dataIn.readUTF(); // readUTF reads a string from the input stream
+            System.out.println("Client: " + clientMessage);
+
+            String serverMessage = "Hello from the server!";
+            dataOut.writeUTF(serverMessage); // writeUTF writes a string to the output stream
+
+            dataIn.close();
+            dataOut.close();
+            clientSocket.close();
+            serverSocket.close();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
